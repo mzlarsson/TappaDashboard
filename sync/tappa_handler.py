618d038d7b7bd6a8bd3c2f1c6ffa73c03c1d43a2
Matz Larsson <matzlarsson94@gmail.com>
2022-09-22 21:06:42 +0200

    General updates for 2022.
    * Updated some class paths
    * Added support for organisation top lists (new feature in tappa)
    * Added support for SQLite (more data!)
    * Some minor adjustments I don't recall

--- sync/tappa_handler.py
+++ sync/tappa_handler.py
@@ -1,14 +1,17 @@
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
+from time import sleep, time
 import json
-import time
+import os
 from datetime import datetime, timezone, timedelta
+import sqlite3
 
+DATA_FOLDER = os.environ.get("DATA_FOLDER", "/data/")
+DB_FILE = os.environ.get("DB_FILE", "/data/data.db")
 
 def print_status(category, msg):
     print("[%s] %s" % (category, msg), flush=True)
 
-def update_data(driver, username, password, folder="/data/"):
+def update_data(driver, username, password):
     driver.get("https://tappa.se")
     assert("Tappa.se" in driver.title)
 
@@ -36,14 +39,52 @@ def update_data(driver, username, password, folder="/data/"):
 
     # Wait for all to load
     print_status("LOGIN", "Waiting for login...")
-    sleep(3)
+    sleep(2)
+    
+    print_status("DATA", "Going to tab Topplistor")  # 5th tab, since first is invis
+    tab_link = driver.find_element_by_css_selector(".tappa-nav-tabs li:nth-child(5) a")
+    tab_link.click()
+
+    sleep(5)
+    
+    print_status("DATA", "Opening table")
+    showall_buttons = driver.find_elements_by_css_selector(".btn.btn-tappa.showall")
+    if len(showall_buttons) != 4:
+        print("Unexpected number of 'showall' buttons ({} but expected 4)".format(len(showall_buttons)))
+    showall_buttons[3].click()
+    
+    sleep(2)
+    
+    org_results = []
+    rows = driver.find_elements_by_css_selector(".modal.fade.in .modal-dialog table.tappa-toplist tbody tr:not(.nodelete)")
+    for row in rows:
+        cells = row.find_elements_by_css_selector("td")
+        org_results.append({
+            "name": cells[1].text,
+            "steps": int(cells[2].text),
+            "distance": float(cells[3].text)
+        })
+        print_status("DATA", "Added organisation %s" % cells[1].text)
+    
+    close_button = driver.find_element_by_css_selector(".modal.fade.in .modal-header button")
+    close_button.click()
+    
+    sleep(4)
+
+    print_status("DATA", "Going to tab Avdelning")  # 6th tab, since first is invis
+    tab_link = driver.find_element_by_css_selector(".tappa-nav-tabs li:nth-child(6) a")
+    tab_link.click()
+
+    sleep(5)
 
     print_status("DATA", "Opening table")
-    showall_button = driver.find_element_by_css_selector(".btn.btn-tappa.showall")
-    showall_button.click()
+    showall_buttons = driver.find_elements_by_css_selector(".btn.btn-tappa.showall")
+    if len(showall_buttons) != 6:
+        print("Unexpected number of 'showall' buttons ({} but expected 6)".format(len(showall_buttons)))
+    showall_buttons[4].click()
 
     # Wait for window to load
-    sleep(1)
+    sleep(2)
 
     def hover_el(el):
         hover = ActionChains(driver).move_to_element(el)
@@ -51,7 +92,7 @@ def update_data(driver, username, password, folder="/data/"):
 
     print_status("DATA", "Iterating over result")
     result = []
-    rows = driver.find_elements_by_css_selector(".modal-dialog table.tappa-toplist tbody tr:not(.nodelete)")
+    rows = driver.find_elements_by_css_selector(".modal.fade.in .modal-dialog table.tappa-toplist tbody tr:not(.nodelete)")
     for row in rows:
         # Load team tooltip
         hover_el(row)
@@ -76,17 +117,99 @@ def update_data(driver, username, password, folder="/data/"):
         print_status("DATA", "Added player %s" % cells[1].text)
 
     print_status("SAVE", "Writing data to file")
-    out_file = "%s/result.json" % folder
     save_data = {
-        "timestamp": int(time.time()),
-        "players": result
+        "timestamp": int(time()),
+        "players": result,
+        "organistaions": org_results
     }
+
+    store_data(save_data)
+
+def store_data(data):
+    store_data_to_file(data)
+    store_data_to_db(data)
+
+def store_data_to_file(data):
+    out_file = "%s/result.json" % DATA_FOLDER
     with open(out_file, "w") as out:
-        json.dump(save_data, out, indent=4)
+        json.dump(data, out, indent=4)
     print_status("SAVE", "File written to %s" % out_file)
 
     one_hour_ago = (datetime.now(timezone(timedelta(hours=1))) - timedelta(hours=1))
-    daily_file = "%s/%s.json" % (folder, one_hour_ago.strftime("%Y%m%d"))
+    daily_file = "%s/%s.json" % (DATA_FOLDER, one_hour_ago.strftime("%Y%m%d"))
     with open(daily_file, "w") as out:
-        json.dump(save_data, out, indent=4)
+        json.dump(data, out, indent=4)
     print_status("SAVE", "Saved to daily file as well")
+
+def store_data_to_db(data):
+    conn = sqlite3.connect(DB_FILE)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            team TEXT
+        );
+    """)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS stats (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            steps INTEGER,
+            distance INTEGER,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS org (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        );
+    """)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS orgstats (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            org_id INTEGER,
+            steps INTEGER,
+            distance INTEGER,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    name_to_user_id = {}
+    users = conn.execute("SELECT id, name FROM user")
+    for user_id, name in users:
+        name_to_user_id[name] = user_id
+        
+    org_name_to_id = {}
+    orgs = conn.execute("SELECT id, name FROM org")
+    for org_id, name in orgs:
+        org_name_to_id[name] = org_id
+
+    for player in data["players"]:
+        name = player["name"]
+        if name not in name_to_user_id:
+            print("Creating user with name {} in DB".format(name))
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO user (name, team) VALUES (?, ?)", (name, player["team"]))
+            name_to_user_id[name] = cursor.lastrowid
+
+        user_id = name_to_user_id[name]
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO stats (user_id, steps, distance) VALUES (?, ?, ?)", (user_id, player["steps"], int(player["distance"]*1000)))
+        
+    for org in data["organistaions"]:
+        name = org["name"]
+        if name not in org_name_to_id:
+            print("Creating org with name {} in DB".format(name))
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO org (name) VALUES (?)", (name,))
+            org_name_to_id[name] = cursor.lastrowid
+        org_id = org_name_to_id[name]
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO orgstats (org_id, steps, distance) VALUES (?, ?, ?)", (org_id, org["steps"], int(1000*org["distance"])))
+    conn.commit()
+    
+    print_status("SAVE", "Saved to database")
+
+# SELECT U.name as name, U.team AS team, MAX(S.steps) as steps, MAX(S.distance) as distance FROM user U LEFT JOIN stats S ON S.user_id = U.id GROUP BY U.id;
+
--- sync/update_and_run.sh
+++ sync/update_and_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Starting Tappa sync..."
 cd /app
+echo "-- Installing requirements"
 pip install -r requirements.txt
+echo "-- Running app"
 python3 "app.py"
